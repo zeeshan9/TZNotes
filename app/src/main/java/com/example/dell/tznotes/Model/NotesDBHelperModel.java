3a065c602bc27c30ca35aae504f827f8299e6456
DELL <zeeshanmushtaq76@gmail.com>
2018-08-16 00:41:04 +0500

    new Add collaborator!

--- app/src/main/java/com/example/dell/tznotes/Model/NotesDBHelperModel.java
+++ app/src/main/java/com/example/dell/tznotes/Model/NotesDBHelperModel.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class NotesDBHelperModel extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "tznotesdatabase.db";
 
     private static final String CREATE_TABLE_NOTES = " CREATE TABLE "+
